6abfd7df32842fc18ca0a9624c1db517e52a987b
manhtiendev <manhtiendev1307@gmail.com>
2024-06-13 18:15:39 +0700

    Auth setup

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,21 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <div className=''>Hello world</div>;
+  return (
+    <div className='flex justify-center min-h-full flex-col py-12 sm:px-6 lg:px-8 bg-gray-100'>
+      <div className='sm:mx-auto sm:w-full sm:w-max-md '>
+        <Image
+          alt='Logo'
+          height='48'
+          width='48'
+          className='mx-auto w-auto'
+          src='/images/logo.png'
+        />
+        <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
+          Sign in to your account
+        </h2>
+      </div>
+      {/* Auth Form */}
+    </div>
+  );
 }
